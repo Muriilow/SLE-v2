6cb40dbcf3371a22780905e30a09debf447a2d9e
muriloUFPR <87148235+Muriilow@users.noreply.github.com>
2025-11-13 17:43:51 -0300

    Creating tables with FLOPS likwid

--- graph.py
+++ graph.py
@@ -6,29 +6,20 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 # Run the likwid choosing the version of the C program and the data we want to get
-def run_likwid(arg, version, option, file="tmp_output.txt"):
+def run_likwid(arg, version, option, file):
 
     command = "likwid-perfctr -C 0 -g " + arg + " -f -m -O ./cgSolver" 
 
     # Writing the output from likwid-csv in the file
     try:
         with open(file, "w") as f:
-            result = subprocess.run(command, 
-                                        shell=True,
-                                        input=option,
-                                        stdout=f,
-                                        text=True)
-
-
-        if result.returncode == 0:
-            return result
-        else:
-            print(f"Erro: {result.stderr}")
-            return None
-
+            subprocess.run(command, 
+                            shell=True,
+                            input=option,
+                            stdout=f,
+                            text=True)
     except Exception as err:
         print(f"Erro ao executar comando: {err}")
-        return None
 
 # Get the information and return only the useful ones from a dictionary 
 def extract_output(file, results):
@@ -47,6 +38,8 @@ def extract_output(file, results):
             if line[0] == 'TABLE' and 'Region' in line[1]:
                 current_region = line[1].replace('Region ', '')
 
+            elif current_region and "AVX DP MFLOP/s" in line[0] and len(line) > 1: 
+                continue
             # Check if this region contains an info we might want
             elif current_region and "DP MFLOP/s" in line[0] and len(line) > 1: 
                 try:
@@ -66,11 +59,19 @@ def extract_output(file, results):
 results = {
     'flops': {}
 }
-options1 = ["COM_1.in", "COM_2.in", "COM_3.in", "COM_4.in", "COM_5.in", "COM_6.in", "COM_7.in", "COM_8.in", "COM_9.in", "COM_10.in", "COM_11.in", "COM_12.in"]
-options2 = ["SEM_1.in", "SEM_2.in", "SEM_3.in", "SEM_4.in", "SEM_5.in", "SEM_6.in", "SEM_7.in", "SEM_8.in", "SEM_9.in", "SEM_10.in", "SEM_11.in", "SEM_12.in"]
+flops_dp = []
+options1 = ["COM_1.in", "COM_2.in", "COM_3.in", "COM_4.in", "COM_5.in", "COM_6.in"]
+options2 = ["SEM_1.in", "SEM_2.in", "SEM_3.in", "SEM_4.in", "SEM_5.in", "SEM_6.in"]
 
-file = "tmp_output.txt"
 
-log = run_likwid("FLOPS_DP", "1", options1[0], file)
+x = [32, 64, 128, 256, 512, 1000]
+for file in options1:
+    with open("Dados/" + file, "r") as f: 
+        option = f.read()
+        run_likwid("FLOPS_DP", "1", option, "tmp.txt")
+        extract_output("tmp.txt", results)
+        flops_dp.append(results['flops']['EXEC_1']['dp'])
 
-extract_output(file, results)
+plt.plot(x, flops_dp)
+
+plt.show()
